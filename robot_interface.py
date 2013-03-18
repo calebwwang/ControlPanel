f61f67d717aa9d96939a62db7c6872b8f8f6b7c4
Gregory Shikhman <cornmander@cornmander.com>
2013-03-18 02:49:53 -0400

    More work on GUI. Started work on graph interface.

--- robot_interface.py
+++ robot_interface.py
@@ -1,4 +1,12 @@
 import serial_connection
 
 def motor_move(motor, speed):
-	print("Sent move command to motor %s with speed %s" % (motor, speed))
+	print("Sent move command to motor %s with speed %s" \
+		% (motor, speed))
+
+def robot_move(direction, speed):
+	print("Sent robot move command with direction %s and speed %s" \
+		% (direction, speed))
+
+def emergency_stop():
+	print("e-stop hit")
